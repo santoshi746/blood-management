d6dea5f7e1ae4f08b0198e25d7d1783d7f0e00d4
uma santoshi tanuku <111972093+santoshi746@users.noreply.github.com>
2025-03-05 21:37:59 +0530

    Add files via upload

---
+++ main.php
@@ -0,0 +1,136 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/5.3.0/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="main.css">
+    <title>Hospital</title>
+  </head>
+  <body>
+<!--Navbar start-->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">HOSPITAL</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav me-auto">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="main.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact Us</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About Us</a>
+        </li>
+      </ul>
+      <ul class="navbar-nav">
+      <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Login
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+            <li><a class="dropdown-item" href="./Hospital/login.php">Hospital</a></li>
+            <li><a class="dropdown-item" href="./Reciever/login.php">User</a></li>
+          </ul>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Register
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+            <li><a class="dropdown-item" href="./Hospital/hos.php">Hospital</a></li>
+            <li><a class="dropdown-item" href="./Reciever/user.php">User</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<!--Navbar end-->
+<div class="row">
+    <div class="col-6">
+        <!--Carousel-->
+<div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+  <!-- <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div> -->
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="https://cdn.pixabay.com/photo/2023/11/15/14/28/heart-8390212_640.jpg" class="d-block image-one w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5></h5>
+        <p></p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="https://media.istockphoto.com/id/1350454780/photo/blood-donation-world-blood-donor-day-man-and-woman-after-giving-blood-save-lives-concept.jpg?s=612x612&w=0&k=20&c=_6lMx2-GmDSak99jzmcK0FlHikkLx3OVXPIoFRNrinw=" class="d-block image-one w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5></h5>
+        <p></p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="https://media.istockphoto.com/id/1476567924/photo/world-donor-day-concept-with-hand-holding-paper-red-drop-on-white-background.jpg?s=612x612&w=0&k=20&c=dZf9lELFYQ6rn20aX-2b_EmgjbiwOZ2ldUl9FtsmsnQ=" class="d-block image-one w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5></h5>
+        <p></p>
+      </div>
+    </div>
+  </div>  
+  <!-- <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button> -->
+</div>
+    </div>
+    <div class="col-6">
+        <marquee behavior="scroll" direction="up">
+        Blood donation is a vital act of altruism and <h1>generosity</h1> that plays a crucial role in modern healthcare systems worldwide. It involves voluntarily giving blood, typically through venipuncture, to be used for transfusions or other medical treatments. The donated blood can save lives, improve <h1>health</h1> outcomes, and support complex medical procedures.
+        Firstly, blood donation is essential for maintaining an adequate supply of blood for medical <h1>emergencies</h1>, surgeries, and treatments. Hospitals rely on donated <h1>blood</h1> to treat patients with conditions such as trauma injuries, surgeries, cancer, anemia, and chronic illnesses. Without a steady supply of blood donations, <h1>healthcare</h1> providers would struggle to meet the demand, potentially endangering patients' <h1>lives</h1>.
+        Secondly, blood donation is <h1>safe</h1> and strictly regulated to ensure both donor and recipient safety. <h1>Donors</h1> undergo screening processes to assess their eligibility, including health checks and assessments of risk factors for transmissible <h1>diseases</h1>. These measures minimize the risk of transmitting infections through blood transfusions and <h1>maintain</h1> the quality of donated blood.
+        
+        Thirdly, blood donation is a <h1>humanitarian</h1> act that brings communities together. It allows individuals to contribute directly to the <h1>well-being</h1> of others in need, irrespective of race, religion, or socioeconomic status. Donors often feel a sense of fulfillment and <h1>satisfaction</h1> knowing they have made a tangible difference in someone's life.
+       
+        Moreover, blood donation <h1>campaigns</h1> and awareness initiatives are crucial in <h1>educating</h1> the public about the importance of donating blood regularly. These efforts dispel <h1>myths</h1> and misconceptions about the donation process and encourage more people to <h1>participate</h1>, thereby increasing the blood supply and saving more lives.
+       
+        In conclusion, blood donation is not just a <h1>medical</h1> procedure; it is a compassionate act that embodies the values of empathy and <h1>solidarity</h1>. It bridges gaps in healthcare provision, supports medical advancements, and fosters a sense of community <h1>responsibility</h1>. By donating blood, individuals contribute to the collective effort of ensuring that every patient receives the <h1>lifesaving</h1> treatment they need, when they need it.
+    </marquee>
+    </div>
+</div>
+<!--Carousel-->
+<div class="container ">
+    <div class="row no-gutters">
+        <div class="col-4 mb-lg-12 column">
+            <img class="image-two img-1" src="https://media.istockphoto.com/id/1397310057/vector/blood-donation-illustration-concept-with-blood-bag-world-blood-donor-day.jpg?s=612x612&w=0&k=20&c=El7inS1jvCpaLKHPrXHQLf_9DNvdJWj8bTTU85k96WU=" alt="">
+            <img class="image-two img-2" src="https://cdn.pixabay.com/photo/2017/08/07/13/05/blood-donation-2603649_640.jpg" alt="">
+            <img class="image-two img-3" src="https://cdn.pixabay.com/photo/2016/10/18/08/52/blood-pressure-monitor-1749577_640.jpg" alt="">
+        </div>
+        <div class="col-4 mb-lg-12 column">
+            <img class="image-two" src="https://media.istockphoto.com/id/1266747084/photo/donor-donating-blood.jpg?s=612x612&w=0&k=20&c=UyAsaxCRiohVYLxhq5eOI91H-YWURXvvGwtVYyIFzkY=" alt="">
+            <img class="image-two" src="https://media.istockphoto.com/id/1353693362/photo/blood-donation-blood-donor-with-bandage-after-giving-blood-on-a-white-background-copy-space.jpg?s=612x612&w=0&k=20&c=wA5FI2Af-Zg_bXhLuFfse_6fnqNPPNpsZkwAMsT-DT4=" alt="">
+            <img class="image-two" src="https://media.istockphoto.com/id/1225040889/photo/blood-donorship-young-girl-in-red-t-shirt-hand-taped-with-patch-with-red-heart-after-giving.jpg?s=612x612&w=0&k=20&c=ot-TQ4kmjjxsYCFJHzTu_cZbAMBT0B01mMZO2_Fwg5E=" alt="">
+        </div>
+        <div class="col-4 mb-lg-12 column">
+            <img class="image-two" src="https://media.istockphoto.com/id/1315395944/photo/experienced-phlebotomist-preparing-a-woman-for-blood-draw.jpg?s=612x612&w=0&k=20&c=MX1sfSztBJIiCW0wOqxDWxLuXWOxrlTvOO6azfF2buY=" alt="">
+            <img class="image-two" src="https://media.istockphoto.com/id/641246806/photo/nurse-receiving-blood-from-blood-donor-in-hospital.jpg?s=612x612&w=0&k=20&c=1kz5JE7B1ZNn1cz8slxWxAqfWYNwlH4yKKt2p92EUjw=" alt="">
+            <img class="image-two" src="https://media.istockphoto.com/id/1335544846/photo/senior-human-hand-holding-red-paper-heart-shape-and-red-blood-drop-symbol-on-bandage-for.jpg?s=612x612&w=0&k=20&c=gVXGcm6WOvo-HNqMFztk6QdaQBUr3vDeU1mOfiDrLuI=" alt="">
+        </div>
+    </div>
+</div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.3.0/js/bootstrap.min.js"></script>
+  </body>
+</html>
